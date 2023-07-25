a5dea33f02822c2e776f76e76377c8646dbda9cf
Alysha Reinard <alysha.reinard@gmail.com>
2023-07-25 09:29:33 +0200

    updated formatting for images on noun gender description

--- src/routes/portfolio/nounGenderGame/+page.svelte
+++ src/routes/portfolio/nounGenderGame/+page.svelte
@@ -11,22 +11,25 @@
 </script>
 
 <h2>Nouns have gender?!</h2>
-<p>
+<div class=container>
+<p style="width:80%">
     As a native English speaker, learning French as an adult has not been easy -- One thing I find particularly difficult is noun gender.  
     A table has a gender? Un chat (a cat) is masculine, but une horloge (a clock) is feminine?  What madness is this?  
     A good way to remember the genders of nouns is to hyper-exaggerate the gender -- imaging a french cat with a mustache or a pink princess clock.  
     </p>
-<div class=blueborder>
-    <img width=200 src="https://allthingsd.com/files/2012/06/funny-pictures-french-cat-has-a-moustache.jpeg" alt="cat with beret and mustache"/>
+
+    <img  class=blueborder width=100% src="https://allthingsd.com/files/2012/06/funny-pictures-french-cat-has-a-moustache.jpeg" alt="cat with beret and mustache"/>
+
 </div>
-<div class=pinkborder>
-    <img width=200 src={pinkClock} alt="pink clock"/>
-</div>
-    <p>What I really needed was a game to help me drill noun genders, but I couldn't really find one.  So I created this game with pink and blue boxes for the "boy" nouns and the "girl" nouns.  
+<div class=container>
+
+    <img class=pinkborder width=100% src={pinkClock} alt="pink clock"/>
+
+    <p style="width:80%">What I really needed was a game to help me drill noun genders, but I couldn't really find one.  So I created this game with pink and blue boxes for the "boy" nouns and the "girl" nouns.  
     Drag the words to the correct box, and if you get it right, the word will appear in the bottom box, appropriately colored.  Once you get 15 words correct, you win! 
     Wrong words will appear in the penalty box for two turns and then get put back into the queue.  
 </p>
-
+</div>
 
 <div class=center><button on:click={toggleGame}> start game </button>
 </div>
@@ -54,11 +57,16 @@
     }
 
     .blueborder {
-        border: solid 2px lightblue;
-        width:200px;
+        width:20%;
+        border:5px solid lightblue;
     }
     .pinkborder{
-        border: solid 2px pink;
-        width:200px;
+        border:5px solid pink;
+        width:15%
     }
+    .container {
+        display:flex;
+        justify-content:space-between;
+    }
+
     </style>
--- src/routes/portfolio/nounGenderGame/WordCard.svelte
+++ src/routes/portfolio/nounGenderGame/WordCard.svelte
@@ -1,27 +1,73 @@
+<script>
+	let flipped = false;
+	export let background = 'lightgray';
+
+	function flip(node, { delay = 0, duration = 1000 }) {
+		return {
+			delay,
+			duration,
+			css: (t, u) => `
+				transform: rotateY(${1 - u * 180}deg);
+				opacity: ${1 - u};
+			`
+		};
+	}
+
+	function handleClick() {
+		if ($$slots.backContent) {
+			flipped = !flipped;
+		}
+	}
+
+</script>
+
+<div
+	class="card-container"
+	tabindex="0"
+	role="cell"
+	on:click={handleClick}
+	on:keypress={handleClick}
+>
+	<div class="card">
+		{#if !flipped}
+			<div style:background-color={background} class="side" transition:flip>
+				<h2 class="frontText">
+					<slot name="frontContent">Unknown contents</slot>
+				</h2>
+			</div>
+		{:else}
+			<div class="side back" transition:flip>
+				<h2 class="backText">
+					<slot name="backContent">Unknown back content</slot>
+				</h2>
+			</div>
+		{/if}
+	</div>
+</div>
+
 <style>
 	.backText {
 		text-align: center;
-		color:black;
-		
+		color: black;
 	}
-	.frontText{
+	.frontText {
 		text-align: center;
-		color:black;
+		color: black;
 	}
 	.card-container {
 		width: 100px;
 		height: 50px;
 		position: relative;
-		margin:2%;
+		margin: 2%;
 	}
-	
+
 	.card {
 		width: 100%;
 		height: 100%;
 		position: absolute;
 		perspective: 600;
 	}
-	
+
 	.side {
 		color: black;
 		position: absolute;
@@ -35,59 +81,12 @@
 		justify-content: center;
 		align-items: center;
 	}
-	
+
 	.back {
 		background-color: whitesmoke;
-		border-color:#d6ccd8;
-		color:black;
+		border-color: #d6ccd8;
+		color: black;
 		border-style: solid;
 		border-width: 5px;
 	}
 </style>
-
-<script>
-	let flipped = false;
-	export let background="lightgray";
-	
-	function flip(node, {
-		delay = 0,
-		duration = 1000
-	}) {
-		return {
-			delay,
-			duration,
-			css: (t, u) => `
-				transform: rotateY(${1 - (u * 180)}deg);
-				opacity: ${1 - u};
-			`
-		};
-	}
-
-	function handleClick() {
-		if ($$slots.backContent){
-			flipped = !flipped;
-		}
-	}
-</script>
-
-<div class="card-container" tabindex="0" role="cell" on:click={handleClick} on:keypress={handleClick}>
-	<div class="card">
-		{#if !flipped}
-		<div style:background-color={background} class="side" transition:flip>
-			<h2 class="frontText">
-				<slot name="frontContent">
-					Unknown contents
-				</slot>
-			</h2>
-		</div>
-		{:else}
-		<div class="side back" transition:flip>
-			<h2 class="backText">
-				<slot name="backContent">
-					Unknown back content
-				</slot>
-			</h2>
-		</div>
-		{/if}
-	</div>
-</div>
