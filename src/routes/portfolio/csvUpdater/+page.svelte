d9fc79fc705417004190b3bfa6a68b6d936b3e5c
Alysha Reinard <alysha.reinard@gmail.com>
2023-10-09 09:31:56 +0200

    changed wording for csv file page

--- src/routes/portfolio/csvUpdater/+page.svelte
+++ src/routes/portfolio/csvUpdater/+page.svelte
@@ -129,11 +129,12 @@
 	<h3>How often do you download a csv file and then have to manually remove or rename fields, concatenate values, format phone numbers etc. before uploading the file to another program?</h3>
 	<h3>I can create a web or desktop app that automatically makes the changes you need.  If the apps you use have a good API I can even remove the download and upload steps. </h3>
 	<h3> Here I've created a toy example that lets you select fields you want to keep and those you want to rename.  For each you can then <i>Reverse</i> (reverse the order of the letters or numbers in each cell), <i>Concatenate</i> (add '-' plus the row number to each cell), or leave them unchanged. </h3>
-
+	<h3>I can create a custom version for you that handles any formatting you need.</h3>
 	<h2>Let's clean up that CSV file!</h2>
 
-	<input type="file" bind:this={myfile} accept=".csv" />
-
+	<label for="myfile">Select a file:</label>
+	<input  type="file" id="myfile" name="myfile" bind:this={myfile} accept=".csv" on:select={() => uploadFile(myfile)}/>
+	
 	<button on:click={() => uploadFile(myfile)}> Upload </button>
 </div>
 
@@ -214,5 +215,6 @@
 		align-self: bottom;
 
 	}
+
 
 </style>
