7c23c01ff839bd17cfd457ce08ec78ad1b39e6a8
Alysha Reinard <alysha.reinard@gmail.com>
2023-10-13 12:21:46 +0200

    adding fathom tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,23 @@
     import { page } from '$app/stores';
 	import { PUBLIC_CANONICAL_ORIGIN } from '$env/static/public';
 	import  SeoComponent  from '$component/SeoComponent/SeoComponent.svelte';
+	import { onMount } from 'svelte';
+	import * as Fathom from 'fathom-client';
+	import { PUBLIC_FATHOM_SITE_ID } from '$env/static/public';
 
+	onMount(() => {
+		Fathom.load(PUBLIC_FATHOM_SITE_ID);
+	});
+
+    // Track page view when path changes.
+	$: $page.url.pathname, browser && Fathom.trackPageview();
 </script>
 <SeoComponent
 	url={$page.url}
 	canonical_origin={PUBLIC_CANONICAL_ORIGIN}
 	data={{ title: $page.data.title, description: $page.data.description }}
 />
+
 <Header/>
 
 <main>
