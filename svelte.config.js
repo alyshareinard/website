b357d8041373b7e742adcd3094a810552fc7a17d
Alysha Reinard <alysha.reinard@gmail.com>
2025-01-21 14:49:49 +0100

    removed sentry

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import Cloudflare from 'cloudflare/index.mjs';
 //import preprocess from 'svelte-preprocess';
 import { sveltePreprocess } from 'svelte-preprocess';
 
@@ -9,7 +10,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter(Cloudflare),
 		alias: {
 			$db: './src/lib/db',
 			$component: './src/lib/components',
