03d36c0590de100ec9141f5f227cfcd34f80b484
Alysha Reinard <alysha.reinard@gmail.com>
2025-01-21 12:03:53 +0100

    removed sentry

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "tech-aly",
-            project: "tech-aly-website"
-        }
-    }), sveltekit()], 
+	plugins: [ sveltekit()], 
     optimizeDeps: {
         include: ['@exodus/schemasafe'] // Add this to make client-side validation work
       }
