f0417eee4f8880a9be2bc2d7654beebbb421c712
Alysha Reinard <alysha.reinard@gmail.com>
2025-03-28 12:05:24 +0100

    updated adaptor -- had been cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-import Cloudflare from 'cloudflare/index.mjs';
+import adapter from '@sveltejs/adapter-vercel';
 //import preprocess from 'svelte-preprocess';
 import { sveltePreprocess } from 'svelte-preprocess';
 
@@ -10,7 +9,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(Cloudflare),
+		adapter: adapter(),
 		alias: {
 			$db: './src/lib/db',
 			$component: './src/lib/components',
