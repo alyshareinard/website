2909470d36460b5a7b8f9dbe1e17d18167d284e0
Alysha Reinard <alysha.reinard@gmail.com>
2025-04-24 14:58:59 +0200

    changed redirect to relative path.

--- src/routes/portfolio/spotifyPlaylistMix/spotifyAuth.server.ts
+++ src/routes/portfolio/spotifyPlaylistMix/spotifyAuth.server.ts
@@ -26,7 +26,7 @@ export async function refresh_token(cookies) {
 		cookies.delete('refresh_token', { path: '/' });
 		console.log('Houston, we have a problem ');
 		console.log(response);
-		redirect(303, spotifyRedirectURL + 'login');
+		redirect(303, '../login');
 	}
 
 	return access_token;
